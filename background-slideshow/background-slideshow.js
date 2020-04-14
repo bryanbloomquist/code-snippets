7cfe2ae0baff56b52151f09723fae17a5af858ff
bloomy215 <bmbloomquist@gmail.com>
2020-04-14 18:38:58 -0500

    added background slideshow

---
+++ background-slideshow/background-slideshow.js
@@ -0,0 +1,22 @@
+/**
+ * @param  {number} interval (in milliseconds)
+ */
+function cycleBackgrounds(interval) {
+  let index = 0;
+
+  const $imageEls = document.querySelectorAll(".container .slide"); // Get the images to be cycled.
+
+  setInterval(() => {
+    // Get the next index.  If at end, restart to the beginning.
+    index = index + 1 < $imageEls.length ? index + 1 : 0;
+
+    // Show the next
+    $imageEls[index].classList.add("show");
+
+    // Find the previous.
+    const previous = index - 1 < 0 ? $imageEls.length - 1 : index - 1;
+
+    // Hide the previous
+    $imageEls[previous].classList.remove("show");
+  }, interval);
+}
