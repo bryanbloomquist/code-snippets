55a52f25f4a6998438bacf7b547275db38227525
bryan <bmbloomquist@gmail.com>
2020-06-29 14:58:27 -0500

    couple changes to files

--- -child/functions.php
+++ -child/functions.php
@@ -1,17 +1,13 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-function my_theme_enqueue_styles() {
- 
-    $parent_style = 'parent-style'; 
- 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+
+add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style');
+
+function enqueue_parent_theme_style() {
+  wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
 add_action( 'wp_enqueue_scripts', 'my_custom_script_load' );
+
 function my_custom_script_load(){
   wp_enqueue_script( 'my-custom-script', get_stylesheet_directory_uri() . '/js/my-script.js', array( 'jquery' ) );
 }
