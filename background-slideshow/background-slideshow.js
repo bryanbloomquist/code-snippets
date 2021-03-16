de5e4f274238b54712f9cc21dbdb199f221e5b52
Bryan Bloomquist <bryanbloomquist@Bryans-MacBook-Pro.local>
2021-03-16 13:37:44 -0500

    clean up

--- background-slideshow/background-slideshow.js
+++ background-slideshow/background-slideshow.js
@@ -20,3 +20,24 @@ function cycleBackgrounds(interval) {
     $imageEls[previous].classList.remove("show");
   }, interval);
 }
+
+
+
+//////////Slideshow Through Fishing Images
+let index = 0;
+const fishingImgs = [
+    "https://3989ac5bcbe1edfc864a-0a7f10f87519dba22d2dbc6233a731e5.ssl.cf2.rackcdn.com/realguides/rg-fisherman.png",
+    "https://3989ac5bcbe1edfc864a-0a7f10f87519dba22d2dbc6233a731e5.ssl.cf2.rackcdn.com/realguides/icefishing.png",
+    "https://3989ac5bcbe1edfc864a-0a7f10f87519dba22d2dbc6233a731e5.ssl.cf2.rackcdn.com/realguides/oceanfishing.png"
+];
+//////Preload Background Images
+$.fn.preload = function() {
+    this.each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+$(fishingImgs).preload();
+setInterval(() => {
+    index = index + 1 < fishingImgs.length ? index + 1 : 0;
+    $(".rg-adventures__inner__background--fishing").css('background-image', 'url('+fishingImgs[index]+')');
+}, 3000);
